95ed6a479c56d4640dbc5657aa9d0c83c577e7d9
arty1978 <artyomkin@gmail.com>
2022-11-06 11:19:22 +0200

    add

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
-import { ClientsService } from './clients.service';
+import { Controller, Get, Param, ParseIntPipe } from "@nestjs/common";
+import { ClientsService } from "./clients.service";
 
-@Controller('clients')
+@Controller("clients")
 export class ClientsController {
   @Get()
   clients() {
     return this.clientsService.getClients();
+  }
+
+  @Get("birthdays")
+  birthdays() {
+    return this.clientsService.getBirthdays();
+  }
+
+  @Get(":id")
+  client(@Param("id", new ParseIntPipe()) id: number) {
+    return this.clientsService.getClient(id);
   }
 
   constructor(private clientsService: ClientsService) {}
--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,5 +1,27 @@
-import { Controller } from '@nestjs/common';
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
-export class ClientsService {}
+export class ClientsService {
+  getClients() {
+    return [
+      {
+        id: 1,
+        firstName: "דני",
+        lastName: "לוי",
+      },
+      {
+        id: 2,
+        firstName: "אברהם",
+        lastName: "אבינו",
+      },
+    ];
+  }
+
+  getBirthdays() {
+    return ["2022-01-01", "1980-01-05"];
+  }
+
+  getClient(id: number) {
+    return this.getClients().find((x) => x.id == id);
+  }
+}
